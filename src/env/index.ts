0b7b823b1c05802e889d0574c428710332c1d236
luanrf5g <of.luan.rferreira@gmail.com>
2024-08-05 22:43:48 -0300

    Initial commit: Structure of Application

---
+++ src/env/index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  throw new Error('❌ Invalid environment variables.')
+}
+
+export const env = _env.data
