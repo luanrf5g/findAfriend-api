60b82fd90b49c6124a7625c8a6c9ead211198533
luanrf5g <of.luan.rferreira@gmail.com>
2024-08-21 06:11:13 -0300

    add some tests for register use-case

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,14 +1,21 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+import { compare } from 'bcryptjs'
+import { OrgAlreadyExistsError } from './errors/org-already-exists-error'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: RegisterUseCase
 
 describe('Register Use Case', () => {
-  it('should be able to register a org', async () => {
-    const orgsRepository = new InMemoryOrgsRepository()
-    const registerUseCase = new RegisterUseCase(orgsRepository)
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new RegisterUseCase(orgsRepository)
+  })
 
-    const { org } = await registerUseCase.execute({
+  it('should be able to register a org', async () => {
+    const { org } = await sut.execute({
       author: 'Antonio bandeira',
       email: 'nome@example.com',
       whatsapp: '81992961037',
@@ -22,5 +29,56 @@ describe('Register Use Case', () => {
     })
 
     expect(org.id).toEqual(expect.any(String))
+  })
+
+  it('should hash org password upon registration', async () => {
+    const { org } = await sut.execute({
+      author: 'Antonio bandeira',
+      email: 'nome@example.com',
+      whatsapp: '81992961037',
+      password: '123456',
+      cep: '55016300',
+      adress: 'Rua Texas, 119',
+      city: 'Caruaru',
+      state: 'PE',
+      latitude: -27.587,
+      longitude: -10.597,
+    })
+
+    const isPasswordHashCorrectly = await compare('123456', org.password_hash)
+
+    expect(isPasswordHashCorrectly).toBe(true)
+  })
+
+  it('should not be able to register with email twice', async () => {
+    const email = 'nome@example.com'
+
+    await sut.execute({
+      author: 'Antonio bandeira',
+      email,
+      whatsapp: '81992961037',
+      password: '123456',
+      cep: '55016300',
+      adress: 'Rua Texas, 119',
+      city: 'Caruaru',
+      state: 'PE',
+      latitude: -27.587,
+      longitude: -10.597,
+    })
+
+    await expect(() =>
+      sut.execute({
+        author: 'Antonio bandeira',
+        email: 'nome@example.com',
+        whatsapp: '81992961037',
+        password: '123456',
+        cep: '55016300',
+        adress: 'Rua Texas, 119',
+        city: 'Caruaru',
+        state: 'PE',
+        latitude: -27.587,
+        longitude: -10.597,
+      }),
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
   })
 })
